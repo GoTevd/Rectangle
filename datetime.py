223ee6355821be304cf7a58c0c227f6cda3eda5b
Gocha Tevdoradze <gt.fo7ce@gmail.com>
2020-06-01 22:44:42 +0300

    оставим отделльно определение класса с контруктором

--- datetime.py
+++ datetime.py
@@ -11,9 +11,9 @@ class Rectangle():
     # высота квадрата
     height = 10
     # перерисовать квадрат (метод класса )
-    # def __init__(self,deminsion1,deminsion2):
-    #     self.height = deminsion1
-    #     self.width = deminsion2
+    def __init__(self,deminsion1,deminsion2):
+         self.height = deminsion1
+         self.width = deminsion2
 
 
     def redraw(self,x,y):
@@ -23,30 +23,3 @@ class Rectangle():
         self.width = y
     def getArea(self):# getter геттер - выдает площадь
         return self.width * self.height
-
-Rect = Rectangle()
-print("Rect.height")
-print(Rect.height)
-print("Rect.width")
-print(Rect.width)
-print("Rect.getArea()")
-print(Rect.getArea())
-#здесь состояние (переменной) объекта Rect пользовательского типа Rectangle, было изменено методом redraw()
-Rect.redraw(2,3)#здесь состояние объекта REct класса Rectangle, было изменено методом redraw()
-print("Rect.height")
-print(Rect.height)
-print("Rect.width")
-print(Rect.width)
-print("Rect.getArea()")
-print(Rect.getArea())
-
-#создадим новый объект типа квадрат
-newRect = Rectangle()
-print (newRect.width)
-print (newRect.height)
-newRect.redraw(99,22)
-
-anotherRect = Rectangle(33,44)
-print (newRect.width)
-print (newRect.height)
-newRect.redraw(99,22)
